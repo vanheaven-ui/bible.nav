690a93d484f7c5a5a3d900b1c1b9c59b81c75b7a
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-28 22:45:08 +0300

    Add boolean logic to switch navbars for loggedin and loggedout

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,24 +1,31 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
 import Navbar from './common/Navbar';
+import LoggedInNavbar from './common/NavbarLogin';
 import Login from './Login';
 import Signup from './Signup';
 
-const Routes = () => (
-  <Router>
-    <Navbar />
-    <Switch>
-      <Route exact path="/">
-        <App />
-      </Route>
-      <Route path="/signup">
-        <Signup />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-    </Switch>
-  </Router>
-);
+const Routes = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <Router>
+      { loggedIn && <LoggedInNavbar /> }
+      { !loggedIn && <Navbar /> }
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login update={setLoggedIn} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default Routes;
