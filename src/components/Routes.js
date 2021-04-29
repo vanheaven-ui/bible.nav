2958a519acdf564fac001a1bdbbab7679db78834
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-29 18:41:12 +0300

    Add routes for Book and Verse components

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
+import Book from './Book';
 import Navbar from './common/Navbar';
 import LoggedInNavbar from './common/NavbarLogin';
 import Login from './Login';
 import Signup from './Signup';
+import Verse from './Verse';
 
 const Routes = () => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -22,6 +24,12 @@ const Routes = () => {
         </Route>
         <Route path="/login">
           <Login update={setLoggedIn} />
+        </Route>
+        <Route path="/book">
+          <Book />
+        </Route>
+        <Route path="/verse/:id">
+          <Verse />
         </Route>
       </Switch>
     </Router>
