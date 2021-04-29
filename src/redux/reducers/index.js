0493b269844d5337a41015183e403eb125096c71
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-29 18:42:42 +0300

    Add chapters reducer to the rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import chaptersReducer from './chapter';
 import userReducer from './user';
 
-const rootReducer = combineReducers({ user: userReducer });
+const rootReducer = combineReducers({ user: userReducer, chapter: chaptersReducer });
 
 export default rootReducer;
