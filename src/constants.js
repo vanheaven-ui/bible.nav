fecce12ed9cafc87bb4a913e6b40f9453e687fae
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-29 13:20:23 +0300

    Define external API request URLs

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://api.scripture.api.bible/v1/bibles/55212e3cf5d04d49-01';
+
+export const BOOKS_URL = `${BASE_URL}/books?include-chapters=true&include-chapters-and-sections=true`;
