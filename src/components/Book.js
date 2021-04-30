b2a8742d628b92b4998a9d6c8c3c1bcd6e9eca68
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-30 13:32:42 +0300

    Display a list of chapters and verses

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,32 +1,79 @@
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
+import chaptersURL from '../constants';
+import fetchData from '../data/apiRequests';
+import { getChapterID, getVerses } from '../redux/actions';
+import { getVerseNumbers } from '../redux/reducers/selectors';
 
 const Book = () => {
   // Get chapters from Redux store using useSelector hook
   const chapters = useSelector(state => state.chapter);
 
+  // useParams to get the ID of the Chapter
+  const { id } = useParams();
+
+  // useDispatch to dispatch getVerses action
+  const dispatch = useDispatch();
+
+  // variables to handle verses
+  const [verses, setVerses] = useState([]);
+  console.log(verses);
+
   // useHistory to browse to to verse page/component
   const hist = useHistory();
 
-  // Method to load verse on Click
+  // Method to load verses on Click
   const loadVerse = e => {
-    console.log(e.target.id);
-    hist.push('/verse');
+    dispatch(getChapterID(e.target.id));
+    fetchData(chaptersURL(e.target.id))
+      .then(res => res.json())
+      .then(data => {
+        setVerses(getVerseNumbers(data.data.content));
+        dispatch(getVerses(data.data.content));
+      });
+  };
+
+  // get specific verse on click
+  const getVerse = e => {
+    hist.push(`/books/${id}/verses/${e.target.id}`);
   };
 
   return (
-    <div className="chapter-list">
-      { chapters && chapters.map(chapter => (
-        <button
-          type="button"
-          id={chapter.id}
-          key={chapter.id}
-          onClick={e => loadVerse(e)}
-        >
-          {chapter.number}
-        </button>
-      ))}
-    </div>
+    <section className="book-details">
+      <div className="chapter-list">
+        <span>
+          {`${id}`}
+          {' '}
+          Chapters
+        </span>
+        {' '}
+        { chapters && chapters.map(chapter => (
+          <button
+            type="button"
+            id={chapter.id}
+            key={chapter.id}
+            onClick={e => loadVerse(e)}
+          >
+            {chapter.number}
+          </button>
+        ))}
+      </div>
+      <div className="verse-list">
+        <span>Verses: </span>
+        {' '}
+        { verses && verses.map(verse => (
+          <button
+            type="button"
+            id={verse}
+            key={verse}
+            onClick={e => getVerse(e)}
+          >
+            {verse}
+          </button>
+        ))}
+      </div>
+    </section>
   );
 };
 
