fbaff5046b7a92876b6535f0ebaf780724f6389f
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-30 13:33:24 +0300

    Display the requested verse

--- src/components/Verse.js
+++ src/components/Verse.js
@@ -1,16 +1,30 @@
 // import { useEffect } from 'react';
+// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import chaptersURL from '../constants';
+import { getVerse } from '../redux/reducers/selectors';
 // import fetchData from '../data/apiRequests';
 
 const Verse = () => {
-  const { id } = useParams();
-  console.log(chaptersURL(id));
-  console.log(id);
+  const { id, verse } = useParams();
+  console.log(id, verse);
+
+  // Grab the chapterId and the data from Redux store
+  const chapterID = useSelector(state => state.chapterId);
+  const details = useSelector(state => state.verses);
+
+  // Create a verse id to match against the data
+  const verseID = `${chapterID}.${verse}`;
+  console.log(verseID);
+
+  // Get the preferred verse
+  getVerse(details, verseID);
 
   return (
     <div className="verse">
       Working
+      <h2>{verseID}</h2>
+      <p>{getVerse(details, verseID)}</p>
     </div>
   );
 };
