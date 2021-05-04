4ad2872105be68c41f3362ba3afdb0a741be85f1
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-04 11:34:30 +0300

    Add boostrap module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Routes from './components/Routes';
 
