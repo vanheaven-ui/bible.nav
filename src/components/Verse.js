545521c19ea14bf3b38b0e623c898726aca05091
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-05 12:25:20 +0300

    Implement verse component and amend data request process

--- src/components/Verse.js
+++ src/components/Verse.js
@@ -1,25 +1,48 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { getVerse } from '../redux/selectors';
+import { RAPID_API_BASE } from '../constants';
+import '../styles/verse.css';
 
 const Verse = () => {
   // Get route parameters using useParams hook
   const { id, verse } = useParams();
+  const [text, setText] = useState('');
   console.log(id, verse);
+
+  // stte variables to manange verse
+  const [scripture, setScripture] = useState({});
+  console.log(scripture);
 
   // Grab the chapterId and the data from Redux store
   const { chapterID, chapterNum } = useSelector(state => state.chapterId);
-  const details = useSelector(state => state.verses);
   const bookName = useSelector(state => state.name);
   const { jwt: token, user } = useSelector(state => state.user);
   console.log(bookName, chapterNum);
   const userId = user.id;
   console.log(token);
 
+  // Get verse chosen from external api
+  useEffect(() => {
+    fetch(`${RAPID_API_BASE}?Verse=${verse}&chapter=${chapterNum}&Book=${bookName}`, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '36b571e37emshf64ca4aee9ccebcp1eeaefjsn46401cd9bc4a',
+        'x-rapidapi-host': 'ajith-holy-bible.p.rapidapi.com',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        setText(data.Output);
+        setScripture(data);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
   // Create a verse id to match against the data
   const verseID = `${chapterID}.${verse}`;
   console.log(verseID);
-  console.log(`https://biblenav-api.herokuapp.com/api/v1/users/${userId}/favorites`);
+
   // Add to favorites on click
   const addFavorite = () => {
     fetch(`https://biblenav-api.herokuapp.com/api/v1/users/${userId}/favorites`, {
@@ -30,12 +53,7 @@ const Verse = () => {
         Authorization: `Bearer ${token}`,
         withCredentials: true,
       },
-      body: JSON.stringify({
-        book_name: bookName,
-        chapter_num: chapterNum,
-        verse_num: verse,
-        verse: getVerse(details, verseID),
-      }),
+      body: JSON.stringify(scripture),
     })
       .then(res => res.json())
       .then(data => console.log(data))
@@ -43,7 +61,7 @@ const Verse = () => {
   };
 
   return (
-    <div className="verse">
+    <section className="verse">
       <div id="google_translate_element" />
       <h2>{`${bookName}`}</h2>
       <h3>
@@ -55,14 +73,14 @@ const Verse = () => {
         {' '}
         {verse}
       </h3>
-      <p>{getVerse(details, verseID)}</p>
+      <p>{text}</p>
       <button
         type="button"
         onClick={() => addFavorite()}
       >
         Add to favourites(Should use a star)
       </button>
-    </div>
+    </section>
   );
 };
 
